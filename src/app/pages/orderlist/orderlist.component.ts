2f42b91f771cb42ddb1ac6e578dea8e652a10bda
Mabdulah <Mabdulah596@hotmail.com>
2019-02-14 16:45:34 +0500

    update

--- src/app/pages/orderlist/orderlist.component.ts
+++ src/app/pages/orderlist/orderlist.component.ts
@@ -43,24 +43,7 @@ export class OrderlistComponent implements OnInit {
 
     // this.selectToday();
   }
-  print(): void {
-    let printContents, popupWin;
-    printContents = document.getElementById('print-section').innerHTML;
-    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
-    popupWin.document.open();
-    popupWin.document.write(`
-      <html>
-        <head>
-          <title>Print tab</title>
-          <style>
-          //........Customized style.......
-          </style>
-        </head>
-    <body onload="window.print();window.close()">${printContents}</body>
-      </html>`
-    );
-    popupWin.document.close();
-}
+ 
 datePicker(){
  this.set=0;
  this.set1=0;
